3f46316a0bf80d081fec5a5ecc8b175392e27b83
iceberg <iceberg@localhost>
2024-06-14 15:39:08 +0530

    hi....

---
+++ src/main/java/com/example/demoA/Person.java
@@ -0,0 +1,45 @@
+package com.example.demoA;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/test/java/com/example/demoA/DemoAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
